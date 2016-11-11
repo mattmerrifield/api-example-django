4428389c9009d0328a67a3e5220093267d1eddfb
matt <rmatthewmerrifield@gmail.com>
2016-11-10 17:44:12 -0800

    docstrings

--- drchrono/serializers.py
+++ drchrono/serializers.py
@@ -22,22 +22,18 @@ class AppointmentSerializer(ModelSerializer):
         fields = ('id', 'patient', 'doctor', 'status', 'scheduled_time', 'duration')
         depth = 1
 
+    # We push the update/create responsibility to the API resource layer so the serializers don't need to know how to
+    # connect to the API at all; instead they just handle the data
     def create(self, validated_data):
         """
         Only allows appointment creation if the referenced Doctor and Patient are already in the database
 
         Raises Doctor.DoesNotExist or Patient.DoesNotExist when they aren't.
         """
-        # We push this responsibility to the API resource layer so the serializers don't need to know how to
-        # connect to the API at all; instead they just handle the data
-        #
-        # This will raise Doctor.DoesNotExist
         doctor_id = validated_data.pop('doctor')
         validated_data['doctor'] = Doctor.objects.get(id=doctor_id)
-        # This will raise Patient.DoesNotExist
         patient_id = validated_data.pop('patient')
         validated_data['patient'] = Patient.objects.get(id=patient_id)
-        # Only works if everything is already hunky-dory
         Appointment.objects.create(**validated_data)
 
     def update(self, instance, validated_data):
@@ -46,16 +42,11 @@ class AppointmentSerializer(ModelSerializer):
 
         Raises Doctor.DoesNotExist or Patient.DoesNotExist when they aren't.
         """
-        # We push this responsibility to the API resource layer so the serializers don't need to know how to
-        # connect to the API at all; instead they just handle the data
-        #
-        # This will raise Doctor.DoesNotExist
         doctor_id = validated_data.pop('doctor')
         validated_data['doctor'] = Doctor.objects.get(id=doctor_id)
-        # This will raise Patient.DoesNotExist
         patient_id = validated_data.pop('patient')
         validated_data['patient'] = Patient.objects.get(id=patient_id)
         # Only works because the model is so simple.
         for field_name in self.fields:
-                setattr(instance, field_name, validated_data[field_name])
+            setattr(instance, field_name, validated_data[field_name])
         instance.save()
